d10b6f477829174690c7e99b482303f03b4f7b41
ProgressAce <daiyaansallie7@gmail.com>
2024-03-29 15:50:13 +0200

    refractor: change env configuration implementation
    No longer using an argument passed to the DBClient class to determine the
    database name. More convenient to use three different config files which
    each manage the configuration options for each respective environment:
    prod for production, dev for development and testing for testing.

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,72 +1,42 @@
 // Handles connection and interaction with the mongodb client
 
 const MongoClient = require('mongodb').MongoClient;
+const { config } = require('dotenv');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
 const DB_HOST = process.env.DB_HOST || '127.0.0.1';
 const DB_PORT = process.env.DB_PORT || '27017';
 const url = `mongodb://${DB_HOST}:${DB_PORT}`;
-let DATABASE = null;
+
+let envConfig = null;
+try {
+    const env = process.env.NODE_ENV || 'dev';
+    envConfig = require(`./config.${env}.js`);
+} catch (error) {
+    console.log("No config file for the specified environment.")
+}
 
 const dbCollections = ['cooking', 'baking'];
-
-// DELETE, just for checking mongoclient methods
-//this.client = new MongoClient(uri).db();
 
 // Handles the Mongo database(db) client connection
 class DBClient {
     constructor () {
-        this.client = null;
         this.db = null;
-
-        if (process.env.MODE === 'prod') {
-            DATABASE = 'mycookbook-db';
-        } else if (process.env.MODE === 'dev') {
-            DATABASE = 'mycookbook-dev-db';
-        } else {
-            DATABASE = 'mycookbook-tester-db';
-        }
     }
 
+    /**
+     * Setup the Database client connection.
+     * Connects to the database according to the environment's configuration
+     */
     async connect() {
         try {
-            this.client = await MongoClient.connect(url);
-            this.db = this.client.db(DATABASE);
+            const client = await MongoClient.connect(url);
+            this.db = client.db(envConfig.DB_NAME);
             console.log('Database connection successful.');
         } catch (error) {
             this.db = false;
             console.log(`Error connecting to DB Client: ${error.message}`);
         }        
-    }
-
-    /**
-     * Provides an in-memory database interface for testing purposes.
-     */
-    async connectTestDb() {
-        try {
-            this.mongoServer = new MongoMemoryServer();
-            const mongoUri = await mongoServer.getUri();
-            const client = new MongoClient(mongoUri);
-            await client.connect();
-            this.db = client.db(DATABASE);
-        } catch (error) {
-            this.db = false;
-            console.log(`Error connecting in-memory test DB: ${error.message}`);;
-        }
-    }
-
-    /**
-     * Disconnects the in-memory database interface for testing purposes.
-     */
-    async disconnectTestDb() {
-        if (this.mongoServer) {
-            try {
-                await this.mongoServer.cleanup();
-                await this.mongoServer.stop();
-            } catch (error) {
-                console.log(`Error disconnecting in-memory test DB: ${error.message}`);
-            }
-        }
     }
 
     /**
@@ -78,7 +48,7 @@ class DBClient {
     }
 }
 
-const dbClient = new DBClient(DATABASE);
+const dbClient = new DBClient();
 
 module.exports = {
     DBClient,
