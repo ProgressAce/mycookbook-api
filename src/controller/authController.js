ac01eefda55797d76c5b6e69e875d09fba1be077
ProgressAce <daiyaansallie7@gmail.com>
2024-04-01 04:36:01 +0200

    feat: add controller and services for registration
    The controller and services layer for user registration.
    The controller uses the arguments of the request body to be used as the
    data of the new user. The arguments of the request body are validated
    by middleware meant to validate the arguments for required for
    registration.
    
    The services for defines methods for the controller layer doing
    exactly as their name implies:
    isValidEmail, isValidPassword, findUser, generateFriendlyId and insertUser.

---
+++ src/controller/authController.js
@@ -0,0 +1,62 @@
+// Provides functionality for all authentication endpoints
+const bcrypt = require('bcrypt');
+const authService = require('../services/authServices');
+
+/**
+ * Registers a new user into the database.
+ * Sends status code:
+ * 201 for successful registration of a user.
+ * @param {Request} req the http request interface
+ * @param {Response} res the http response interface
+ */
+const user_registration = async (req, res) => {
+    const { name, username, email } = req.body;
+    const { password, confirmedPwd } = req.body;
+
+    // all args from req.body expected to be validated with authentication
+    // Middleware validation.
+
+    let hashedPwd = null;
+    if (password === confirmedPwd) {
+        try {
+            hashedPwd = bcrypt.hashSync(password, 10);
+        } catch (error) {
+            console.log(`Error while hashing password: ${error}`);
+            return res.status(500).json({error: `Error while hashing password: ${error}`});
+        }
+    } else {
+        return res.status(400).json({error: 'Password and confirmed passsword do not match'});
+    }
+
+    const friendlyId = authService.generateFriendlyId(username);
+
+    let hashedFriendlyId = null;
+    try {
+        hashedFriendlyId = bcrypt.hashSync(friendlyId, 10);
+    } catch (error) {
+        console.log(`Error while hashing friendly ID: ${error}`);
+        return res.status(500).json({error: `Error while hashing friendly ID: ${error}`});
+    }
+
+    const user = {
+        name,
+        username,
+        email,
+        hashedPwd,
+        hashedFriendlyId,
+        role: 'normal',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    }
+
+    const result = await authService.insertUser(user);
+    if (!result) {
+        return res.status(500).json({error: 'Server-side error trying to register new user'});
+    }
+
+    res.status(201).json({message: 'User registered successfully.', friendlyId});
+}
+
+module.exports = {
+    user_registration
+}
